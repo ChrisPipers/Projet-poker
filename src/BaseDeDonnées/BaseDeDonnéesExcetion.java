aad70cc3e904026d7f09450ec0e9dde651cc895b
Mitch <Mitch@192.168.1.4>
2017-08-09 14:17:37 +0200

    creat a exception as the game exception

---
+++ src/BaseDeDonnées/BaseDeDonnéesExcetion.java
@@ -0,0 +1,17 @@
+package BaseDeDonnées;
+
+/**
+ *
+ * @author Mitch
+ */
+public class BaseDeDonnéesExcetion extends Exception {
+
+    public BaseDeDonnéesExcetion() {
+    }
+    
+    public BaseDeDonnéesExcetion(String msg){
+        super(msg);
+    }
+    
+    
+}
