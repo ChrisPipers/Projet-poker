6056d41dc4479a1db311bd26fd32245c8e7e23ad
Mitch <Mitch@192.168.1.4>
2017-08-23 12:45:38 +0200

    add the method for make a transaction with the BD

--- src/baseDeDonnées/DriverManagerP.java
+++ src/baseDeDonnées/DriverManagerP.java
@@ -1,5 +1,6 @@
 package baseDeDonnées;
 
+import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -24,4 +25,68 @@ public class DriverManagerP {
         }
         return conn;
     }
+    /**
+     * start a transaction and determine the level of isolation
+     * 
+     * @param lvl, is the level of the isolation of 
+     * @throws baseDeDonnées.BaseDeDonnéesExcetion
+     * @throws java.lang.ClassNotFoundException
+     */
+    public static void startTransaction(int lvl) throws BaseDeDonnéesExcetion, ClassNotFoundException{
+        try {
+            getConnection().setAutoCommit(false);
+
+            int isol = 0;
+            switch (lvl) {
+                case 0:
+                    isol = java.sql.Connection.TRANSACTION_READ_UNCOMMITTED;
+                    break;
+                case 1:
+                    isol = java.sql.Connection.TRANSACTION_READ_COMMITTED;
+                    break;
+                case 2:
+                    isol = java.sql.Connection.TRANSACTION_REPEATABLE_READ;
+                    break;
+                case 3:
+                    isol = java.sql.Connection.TRANSACTION_SERIALIZABLE;
+                    break;
+                default:
+                    throw new BaseDeDonnéesExcetion("transation not possible ");
+            }
+            getConnection().setTransactionIsolation(isol);
+        } catch (SQLException ex) {
+            throw new BaseDeDonnéesExcetion("impossible to start the transaction : "+ex.getMessage());
+        }
+    }
+
+    /**
+     * valid the transaction 
+     * @throws baseDeDonnées.BaseDeDonnéesExcetion
+     * @throws java.lang.ClassNotFoundException
+     */
+    public static void validateTransaction() throws BaseDeDonnéesExcetion, ClassNotFoundException {
+        try {
+            getConnection().commit();
+            getConnection().setAutoCommit(true);
+        } catch (SQLException ex) {
+            throw new BaseDeDonnéesExcetion("impossible to validat the transaction : "+ex.getMessage());
+        }
+    }
+
+    /**
+     * cancel the transaction
+     * @throws baseDeDonnées.BaseDeDonnéesExcetion
+     * @throws java.lang.ClassNotFoundException
+     */
+    public static void cancelTransaction() throws BaseDeDonnéesExcetion, ClassNotFoundException  {
+        try {
+            getConnection().rollback();
+            getConnection().setAutoCommit(true);
+        } catch (SQLException ex) {
+            throw new BaseDeDonnéesExcetion("not possible to cancel the transaction : "+ex.getMessage());
+        }
+    }
 }
+    
+    
+
